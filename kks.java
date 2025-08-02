7493fb43a5a0b3c91d40b105f337194c4ece4118
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:19:07 +0600

    Add files via upload

---
+++ kks.java
@@ -0,0 +1,21 @@
+public class kks {
+    public static void main(String[] args) {
+      String name="kowshik";
+      System.out.println(name);
+
+      int a=45;
+      float b=45.55f;
+       boolean isAdult=false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(isAdult);
+
+        byte u=56;
+        System.out.println(b);
+        double d=45.3333333d;
+        System.out.println(d);
+        char grade='A';
+         System.out.println(grade);
+        
+    }
+    }
