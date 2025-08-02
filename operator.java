dee0455bed9ca92c19b2ba1d1d02bde5ce02e807
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:23:33 +0600

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,46 @@
+public class operator{
+    public static void main(String args[]){
+    float num1=45, num2=55;
+    num1+=3;
+    num2-=8;
+
+        System.out.print("the value of num1+NUM2");
+                System.out.println( num1+num2);
+
+                
+        System.out.print("the value of num1+NUM2");
+                System.out.println( num1*num2);
+
+
+        System.out.print("the value of num1+NUM2");
+                System.out.println( num1/num2);
+
+
+        System.out.print("the value of num1+NUM2");
+                System.out.println( num1%num2);
+
+         System.out.println( num1++);
+         System.out.println( ++num1);
+        System.out.println( num1--);
+        System.out.println(--num1);  
+
+
+    /*
+    comparison operators
+    ==
+    !=
+    <
+    >
+    <=
+    >=
+    
+loical operators:
+&&-- return true if both are true
+||---- returns true if any one condition is true
+!--reverse the result from true to false
+**/
+    }
+}
+
+
+
