b0776625515d2912c71bbe6b68e2cf9d24dc8ffa
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:23:09 +0600

    Add files via upload

---
+++ Oop69.java
@@ -0,0 +1,9 @@
+import bank;
+
+
+public class Oops69 {
+    public static void main(String args[]) {
+        bank.Account  account1=new bank.Account();
+       account1.name="kowshik.kumar.sarker";
+    }
+}
