8e5f15d8efbda54458767824072174c50a70edfd
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:18:41 +0600

    Add files via upload

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class input{
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter input:");
+      int input = scan.nextInt();
+//        String input = scan.next();
+        System.out.println(input);
+    }
+    }
