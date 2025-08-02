a1b0ee57b258c89346d3b2dedcb4158f32e32e36
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:21:23 +0600

    Add files via upload

---
+++ math.java
@@ -0,0 +1,23 @@
+public class math {
+    public static void main(String args[]) {
+    int num1=4, num2=7;
+        System.out.println(Math.max(num1,num2));
+        System.out.println(Math.min(num1,num2));
+        System.out.println(Math.sqrt(36));
+//        abs..negative nummber dile negative positive dila positive
+        System.out.println(Math.abs(6));
+
+//        will give random number between 0 t0 1 but not 1
+        System.out.println(Math.random());
+
+//        random between 4 to 8
+        System.out.println( 4+(8-4)*Math.random());
+        System.out.println( 4+(8-4)*Math.random());
+        System.out.println( 4+(8-4)*Math.random());
+        System.out.println( 4+(8-4)*Math.random());
+        System.out.println( 4+(8-4)*Math.random());
+
+// java math class methods ...google
+
+    }
+}
