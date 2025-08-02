09f088782a761a5b1c6ceac0dd820706059d0740
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-08-02 23:20:29 +0600

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,17 @@
+public class loop {
+    public static void main(String args[]){
+//loops
+//        int i=0;
+//        while (i<100){
+//            System.out.println(i);
+//            i+=1;
+//        }
+//        do{
+//            System.out.println(i);
+//            i+=1;
+//        }while(i<100);
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
+    }
+}
